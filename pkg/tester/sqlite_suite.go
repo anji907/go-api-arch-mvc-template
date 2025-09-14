cea310531dd76eb9062da7f575b07326f082ed32
anji907 <anji.sk129@gmail.com>
2025-09-14 17:52:56 +0900

    テスト前後で実行したい処理を実装

---
+++ pkg/tester/sqlite_suite.go
@@ -0,0 +1,33 @@
+package tester
+
+import (
+	"os"
+
+	"github.com/stretchr/testify/suite"
+
+	"go-api-arch-mvc-template/app/models"
+	"go-api-arch-mvc-template/configs"
+)
+
+type DBSQLiteSuite struct {
+	suite.Suite
+}
+
+// テスト前に自動で実行されるsuiteパッケージの機能
+func (suite *DBSQLiteSuite) SetupSuite() {
+	configs.Config.DBName = "unittest.sqlite"
+	err := models.SetDatabase(models.InstanceSqlLite)
+	suite.Assert().Nil(err)
+
+	for _, model := range models.GetModels() {
+		err := models.DB.AutoMigrate(model)
+		suite.Assert().Nil(err)
+	}
+}
+
+// テスト後に実行されるsuiteパッケージの機能
+func (suite *DBSQLiteSuite) TearDownSuite() {
+	// データベースファイルを削除
+	err := os.Remove(configs.Config.DBName)
+	suite.Assert().Nil(err)
+}
